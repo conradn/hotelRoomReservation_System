234e5a04f2354c22ebe61e1da1f781f5a34ea702
Samuel Teko <samttekoh@gmail.com>
2022-07-10 18:26:39 +0300

    first server commit

---
+++ HotelServer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rmi_hotel_mgt;
+
+/**
+ *
+ * @author HP
+ */
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry; 
+
+
+public class HotelServer {
+     public static void main(String[] args) throws RemoteException{
+        try{
+            Registry port = LocateRegistry.createRegistry(1090);
+            port.rebind("hello",new RoomManagerImpl());
+        }catch (RemoteException e){
+            System.out.println("Something is wrong:" + e);
+        }
+        finally{
+             System.out.print("Server has been triggered");
+        }
+    }
+}
